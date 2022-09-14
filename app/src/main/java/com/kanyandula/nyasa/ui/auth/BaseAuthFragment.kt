59616c052b0830834878140e83b05dfa3e13907e
Ephraim Kanyandula <ekanyandula8@gmail.com>
2022-09-14 15:41:50 +0100

    password reset and redirection done

---
+++ app/src/main/java/com/kanyandula/nyasa/ui/auth/BaseAuthFragment.kt
@@ -0,0 +1,41 @@
+package com.kanyandula.nyasa.ui.auth
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+
+@OptIn(ExperimentalCoroutinesApi::class)
+@AndroidEntryPoint
+abstract class BaseAuthFragment: Fragment() {
+
+    val TAG: String = "AppDebug"
+
+
+
+    private val viewModel: AuthViewModel by activityViewModels()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- app/src/main/java/com/kanyandula/nyasa/util/Constants.kt
+++ app/src/main/java/com/kanyandula/nyasa/util/Constants.kt
@@ -5,6 +5,8 @@ class Constants {
     companion object{
 
         const val BASE_URL = "https://nyasablog.com/api/"
+        const val PASSWORD_RESET_URL: String = "https://nyasablog.com/password_reset/"
+
         const val NETWORK_TIMEOUT = 3000L
         const val TESTING_NETWORK_DELAY = 0L // fake network delay for testing
         const val TESTING_CACHE_DELAY = 0L // fake cache delay for testing
