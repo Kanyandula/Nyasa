5eaf892f2b7211330d5ad5cfd1307aa8ed697894
Ephraim Kanyandula <ekanyandula8@gmail.com>
2022-10-01 21:51:48 +0100

    Password Update

--- app/src/main/java/com/kanyandula/nyasa/ui/main/account/AccountViewModel.kt
+++ app/src/main/java/com/kanyandula/nyasa/ui/main/account/AccountViewModel.kt
@@ -49,7 +49,14 @@ constructor(
                 }?: AbsentLiveData.create()
             }
             is ChangePasswordEvent ->{
-                return AbsentLiveData.create()
+                return sessionManager.cachedToken.value?.let { authToken ->
+                    accountRepository.updatePassword(
+                        authToken,
+                        stateEvent.currentPassword,
+                        stateEvent.newPassword,
+                        stateEvent.confirmNewPassword
+                    )
+                }?: AbsentLiveData.create()
             }
             is None ->{
                 return AbsentLiveData.create()
--- app/src/main/java/com/kanyandula/nyasa/ui/main/account/ChangePasswordFragment.kt
+++ app/src/main/java/com/kanyandula/nyasa/ui/main/account/ChangePasswordFragment.kt
@@ -1,12 +1,17 @@
 package com.kanyandula.nyasa.ui.main.account
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.Observer
+import androidx.navigation.fragment.findNavController
 import com.kanyandula.nyasa.R
-import com.kanyandula.nyasa.ui.main.account.BaseAccountFragment
+import com.kanyandula.nyasa.ui.main.account.state.AccountStateEvent
+import com.kanyandula.nyasa.util.SuccessHandling.Companion.RESPONSE_PASSWORD_UPDATE_SUCCESS
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.android.synthetic.main.fragment_change_password.*
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 
 @OptIn(ExperimentalCoroutinesApi::class)
@@ -24,5 +29,43 @@ class ChangePasswordFragment : BaseAccountFragment(){
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        update_password_button.setOnClickListener {
+            viewModel.setStateEvent(
+                AccountStateEvent.ChangePasswordEvent(
+                    input_current_password.text.toString(),
+                    input_new_password.text.toString(),
+                    input_confirm_new_password.text.toString()
+                )
+            )
+        }
+
+        subscribeObservers()
+    }
+
+    private fun subscribeObservers(){
+        viewModel.dataState.observe(viewLifecycleOwner, Observer{ dataState ->
+            stateChangeListener.onDataStateChange(dataState)
+            Log.d(TAG, "ChangePasswordFragment, DataState: $dataState")
+            if(dataState != null){
+                dataState.data?.let { data ->
+                    data.response?.let{ event ->
+                        if(event.peekContent()
+                                .message
+                                .equals(RESPONSE_PASSWORD_UPDATE_SUCCESS)
+                        ){
+                            findNavController().popBackStack()
+                        }
+                    }
+                }
+            }
+        })
     }
 }
+
+
+
+
+
+
+
