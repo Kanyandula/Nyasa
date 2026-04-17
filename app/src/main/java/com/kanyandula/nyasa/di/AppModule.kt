416577d951104297b52b4760586a2535760c6482
admin <ekanyandula8@gmail.com>
2026-04-17 12:37:51 +0100

    hardening(h1): add kotlinx-serialization, remove Gson converter

--- app/src/main/java/com/kanyandula/nyasa/di/AppModule.kt
+++ app/src/main/java/com/kanyandula/nyasa/di/AppModule.kt
@@ -5,8 +5,7 @@ import android.content.SharedPreferences
 import androidx.room.Room
 import androidx.security.crypto.EncryptedSharedPreferences
 import androidx.security.crypto.MasterKey
-import com.google.gson.Gson
-import com.google.gson.GsonBuilder
+import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
 import com.kanyandula.nyasa.BuildConfig
 import com.kanyandula.nyasa.api.AuthenticationInterceptor
 import com.kanyandula.nyasa.api.TokenInterceptor
@@ -20,10 +19,11 @@ import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import kotlinx.serialization.json.Json
+import okhttp3.MediaType.Companion.toMediaType
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
 import javax.inject.Singleton
 
 @Module
@@ -53,12 +53,6 @@ object AppModule {
 
     @Singleton
     @Provides
-    fun provideGsonBuilder(): Gson {
-        return GsonBuilder().excludeFieldsWithoutExposeAnnotation().create()
-    }
-
-    @Singleton
-    @Provides
     fun provideOkHttpClient(
         tokenInterceptor: TokenInterceptor,
         authenticationInterceptor: AuthenticationInterceptor
@@ -78,11 +72,12 @@ object AppModule {
 
     @Singleton
     @Provides
-    fun provideRetrofitBuilder(gsonBuilder: Gson, okHttpClient: OkHttpClient): Retrofit.Builder {
+    fun provideRetrofitBuilder(okHttpClient: OkHttpClient): Retrofit.Builder {
+        val json = Json { ignoreUnknownKeys = true }
         return Retrofit.Builder()
             .baseUrl(Constants.BASE_URL)
             .client(okHttpClient)
-            .addConverterFactory(GsonConverterFactory.create(gsonBuilder))
+            .addConverterFactory(json.asConverterFactory("application/json".toMediaType()))
     }
 
     @Singleton
